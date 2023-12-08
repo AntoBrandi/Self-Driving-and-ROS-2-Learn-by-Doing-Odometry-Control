a603b3e3483221f8318dee4b6066d925d36bad6e
AntoBrandi <antonio.brandi@outlook.it>
2023-12-08 12:43:54 +0100

    added bumperbot bringup package

---
+++ Section12_Build_the_Robot/bumperbot_ws/src/bumperbot_bringup/launch/real_robot.launch.py
@@ -0,0 +1,40 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    hardware_interface = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_firmware"),
+            "launch",
+            "hardware_interface.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_controller"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "False"
+        }.items(),
+    )
+    
+    joystick = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_controller"),
+            "launch",
+            "joystick_teleop.launch.py"
+        ),
+    )
+    
+    return LaunchDescription([
+        hardware_interface,
+        controller,
+        joystick,
+    ])
