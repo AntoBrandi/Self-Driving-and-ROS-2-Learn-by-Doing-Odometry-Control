20afc595cd7758bdba1865c411eb8c1635ed09af
AntoBrandi <antonio.brandi@outlook.it>
2024-09-25 22:10:43 +0200

    update robot_state_publisher use sim time

--- Section6_Kinematics/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
+++ Section6_Kinematics/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
@@ -42,7 +42,8 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     gazebo = IncludeLaunchDescription(
