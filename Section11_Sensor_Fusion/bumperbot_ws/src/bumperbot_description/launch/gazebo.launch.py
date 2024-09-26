251216d64dba4cc6f64d2107f1068853bdcd1530
AntoBrandi <antonio.brandi@outlook.it>
2024-09-26 23:18:37 +0200

    publish Gazebo clock

--- Section11_Sensor_Fusion/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
+++ Section11_Sensor_Fusion/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
@@ -67,6 +67,7 @@ def generate_launch_description():
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
             "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"
         ],
         remappings=[
